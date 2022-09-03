2c1aca5cf38b8e95d32dbe7e63c57925047d4277
Your Name <you@example.com>
2022-09-03 08:06:45 +0700

    second change

--- Home.php
+++ Home.php
@@ -48,6 +48,8 @@
 
 </head>
 <body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <h1>List Data</h1>
     <div class="tableview">
     <table class="table" >
@@ -72,12 +74,35 @@
                 <!-- <td><img src="<?= $siswa["foto"];?>" style="height: 100px; width: 100px"></td> -->
                 <td>
                     <a type="button" class="btn btn-warning" href="ubah.php? myid= <?=$siswa["id"]?>" >Ubah</a>
+                    <a type="button" class="btn btn-info" href="" data-bs-toggle="modal" data-bs-target="#exampleModal<?$siswa['id']; ?>">Detail</a>
                     <a type="button" class="btn btn-danger" href="hapus.php? myid= <?=$siswa["id"]?> ">Hapus</a>
                 </td>
             </tr>
-        <?php
-            }
-        ?>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal<?$siswa['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <div class="formgroup">
+                <label for="nama"></label>
+                <input type="text" class="form-control" name="nama" value=<?= $datasiswa["nama"] ?>" readonly>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+            <?php
+                }
+            ?>
 
     </table>
     </div>
